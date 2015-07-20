a26bb0cc33b72126df69e0e8a2554acf802e7358
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-07-19 20:39:47 -0400

    get rid of prime boolean, update comments

--- primeSieve.js
+++ primeSieve.js
@@ -1,8 +1,9 @@
-//create an array with numbers 100.
 //Composite < 100 are NOT 2,3,5,7 and divisible by 2,3,5, & 7. 
 // 1 is composite
-var allNumbers = [];
+//Filter out composite #'s
 
+//create an array with numbers 100.
+var allNumbers = [];
 
 for (var i = 1; i <= 100; i++) {
    allNumbers.push(i);
@@ -11,7 +12,7 @@ var lengthArray = allNumbers.length;
 
 //prime number filter
 function primeFilter(numbers) {
-  var prime;
+  
   for (var i = 1; i < lengthArray; i++) {
     
     if (i === 1) {
@@ -29,7 +30,8 @@ function primeFilter(numbers) {
     } else if ((i % 7 !== 0) && (i % 5 !== 0) && (i % 3 !== 0) && (i % 2 !== 0)) {
       console.log(i + ' is prime');
     }
-
+    //push prime numbers to a new array
+    //push composite numbers to a new array (maybe)
   }
 }
 

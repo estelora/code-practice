e8863c96afddde67b00bba8ab30927a04f011cf7
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-07-20 17:21:03 -0400

    completed prime sieve

--- primeSieve.js
+++ primeSieve.js
@@ -1,5 +1,5 @@
 //Composite < 100 are NOT 2,3,5,7 and divisible by 2,3,5, & 7. 
-// 1 is composite
+// skip 1.
 //Filter out composite #'s
 
 //create an array with numbers 100.
@@ -12,27 +12,17 @@ var lengthArray = allNumbers.length;
 
 //prime number filter
 function primeFilter(numbers) {
-  
-  for (var i = 1; i < lengthArray; i++) {
-    
-    if (i === 1) {
-      console.log(i + ' is composite');
-    } else if (i % 2 === 0) {
-      console.log(i + ' is composite');
-    } else if (i % 3 === 0) {
-      console.log(i + ' is composite');
-    } else if (i % 5 === 0) {
-      console.log(i + ' is composite');
-    } else if (i % 7 === 0) {
-      console.log(i + ' is composite');
-    } else if (i === 2 || i === 3 || i === 5 || i === 7) {
-      console.log(i + ' is prime');
+  primes = [];
+  for (var i = 2; i < lengthArray; i++) {
+
+    if (i === 2 || i === 3 || i === 5 || i === 7) {
+      primes.push(i);
     } else if ((i % 7 !== 0) && (i % 5 !== 0) && (i % 3 !== 0) && (i % 2 !== 0)) {
-      console.log(i + ' is prime');
+      primes.push(i);
     }
-    //push prime numbers to a new array
-    //push composite numbers to a new array (maybe)
+
   }
+  console.log(primes);
 }
 
 var primeSieve = primeFilter(allNumbers);

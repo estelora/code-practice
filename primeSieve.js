82b64c5e7ebad40e60ccf75dc571ecccdd186895
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-07-19 20:37:45 -0400

    changed not prime to composite

--- primeSieve.js
+++ primeSieve.js
@@ -1,8 +1,8 @@
 //create an array with numbers 100.
-//Not prime numbers < 100 are NOT 2,3,5,7 and divisible by 2,3,5, & 7. 
-// 1 is not prime
+//Composite < 100 are NOT 2,3,5,7 and divisible by 2,3,5, & 7. 
+// 1 is composite
 var allNumbers = [];
-var sieved = [];
+
 
 for (var i = 1; i <= 100; i++) {
    allNumbers.push(i);
@@ -15,21 +15,21 @@ function primeFilter(numbers) {
   for (var i = 1; i < lengthArray; i++) {
     
     if (i === 1) {
-      console.log(i + ' is not prime');
+      console.log(i + ' is composite');
     } else if (i % 2 === 0) {
-      console.log(i + ' is not prime');
+      console.log(i + ' is composite');
     } else if (i % 3 === 0) {
-      console.log(i + ' is not prime');
+      console.log(i + ' is composite');
     } else if (i % 5 === 0) {
-      console.log(i + ' is not prime');
+      console.log(i + ' is composite');
     } else if (i % 7 === 0) {
-      console.log(i + ' is not prime');
+      console.log(i + ' is composite');
     } else if (i === 2 || i === 3 || i === 5 || i === 7) {
       console.log(i + ' is prime');
     } else if ((i % 7 !== 0) && (i % 5 !== 0) && (i % 3 !== 0) && (i % 2 !== 0)) {
       console.log(i + ' is prime');
     }
-    
+
   }
 }
 

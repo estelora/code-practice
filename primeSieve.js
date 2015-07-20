7a7d960ca6d1af003e6c12f7f1abb61e7069f3b0
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-07-19 20:16:19 -0400

    getting closer, just need to add all the if statements and push to the array

--- primeSieve.js
+++ primeSieve.js
@@ -1,33 +1,26 @@
 //create an array with numbers 100.
+//Not prime numbers < 100 are NOT 2,3,5,7 and divisible by 2,3,5, & 7. 
+// 1 is not prime
 var allNumbers = [];
 
 for (var i = 1; i <= 100; i++) {
    allNumbers.push(i);
 }
-console.log(allNumbers);
 var lengthArray = allNumbers.length;
-console.log(lengthArray);
 
 //prime number filter
 function primeFilter(numbers) {
-
   for (var i = 1; i < lengthArray; i++) {
 
-  // if (i.value == 2 || i.value == 3 || i.value == 5 | i.value == 7) {
-  //   return i;
-  // }
-  // if == 2 || if == 3 || if == 5 || if == 7;
-  //   return i;
-  // if divisible by 2;
-  // if divisible by 3;
-  // if divisible by 5;
-  // if divisible by 7;
-  // else return i;
-
+    if (i === 1) {
+      console.log(i);
+    //add if statements here  
+    }
+  
   }
- 
-  // var sieved = [];
+  
 }
 
-var primeSieve = allNumbers.filter(primeFilter);
-console.log(sieved);
+var primeSieve = primeFilter(allNumbers);
+// console.log(primeSieve);
+

b589d82b654caa17717910c33f4f6a0482d3ea61
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-07-24 21:06:17 -0400

    refine stringReverse.js

--- stringReverse.js
+++ stringReverse.js
@@ -1,12 +1,25 @@
+//Reverse a string
+
 function stringReverse(string) {   
   
-  var back = '';
-  for (var i = string.length - 1; i >= 0; i--)
-  back += string[i];
-  str = back;
-  return string;    
+  //empty string
+  var backString = '';
+  
+  //for loop decrements starting at the back
+  for (var i = string.length - 1; i >= 0; i--) {
+    
+    //return character at the index of i, as we loop
+    backString+= string[i];
+  }
+  
+
+  //return completed back 
+  return backString;
 
 }
+
+
+//Test Cases
 
 stringReverse("backwards");
 stringReverse("palindrome");

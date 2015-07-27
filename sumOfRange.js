bfab0fb41c837b9651ffe8146eb570c90f3cf576
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-07-27 19:14:47 -0400

    finish sumOfRange

--- sumOfRange.js
+++ sumOfRange.js
@@ -3,7 +3,19 @@ function range(start, end) {
   for (i = start; i <= end; i++) {
     num.push(i);
   }
-  console.log(num)
+  console.log(num);
+  return num;
 }
 
 range(1, 10);
+
+function sumOf(array){
+  var sum = 0;
+  for( i = 0; i < array.length; i++) {
+    //each iteration, add the value at i index to sum
+    sum += array[i];
+  }
+  return sum;
+}
+
+console.log(sumOf(range(1, 10)));

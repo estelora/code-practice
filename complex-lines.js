1c5e4515a87b3f08ff0869923ce28beb4a7509ec
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-07-27 15:26:17 -0400

    added complex-lines

---
+++ complex-lines.js
@@ -0,0 +1,12 @@
+for (var line = 1; line <= 5; line++) {
+  for(var j = 1; j <= (-1 * line + 5); j++){
+    console.log('.');
+  }
+  console.log(line);
+}
+// Output:
+// ....1
+// ...2
+// ..3
+// .4
+// 5

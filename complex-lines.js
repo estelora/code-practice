50cf6896bb81aa82773a6735e497abed2c8632f6
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-07-27 15:35:32 -0400

    more problems for complex-lines.js

--- complex-lines.js
+++ complex-lines.js
@@ -4,9 +4,61 @@ for (var line = 1; line <= 5; line++) {
   }
   console.log(line);
 }
+
 // Output:
 // ....1
 // ...2
 // ..3
 // .4
 // 5
+
+for (var line = 1; line <= 5; line ++) {
+  for (var j = 1; j <= (-1 * line + 5); j++) {
+    console.log('.');
+  }
+  for (var k = 1; k <= line; k++) {
+    console.log(line);
+  }
+}
+
+// Output:
+// ....1
+// ...22
+// ..333
+// .4444
+// 55555
+
+for (var line = 1; line <= 5; line ++) {
+  for (var j = 1; j <= (-1 * line + 5); j++) {
+    console.log('.');
+  }
+  console.log(line);
+  for (var j = 1; j <= (line- 1); j++) {
+    console.log('.');
+  }
+}
+
+// Output:
+// ....1
+// ...2.
+// ..3..
+// .4...
+// 5....
+
+for (var line = 1; line <= 5; line ++) {
+  for (var j = 1; j <= (-1 * line + 5); j++) {
+    console.log(' ');
+  }
+  console.log(line);
+  for (var j = 1; j <= (line- 1); j++) {
+    console.log(' ');
+  }
+}
+
+// Output:
+//     1
+//    2 
+//   3
+//  4   
+// 5
+

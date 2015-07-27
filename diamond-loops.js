cd0eefdc860f44a2cd72240c3a09302dc04a2170
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-07-27 16:26:15 -0400

    complete diamond-loops.js

--- diamond-loops.js
+++ diamond-loops.js
@@ -1,19 +1,24 @@
-
 //top half of triangle, lines 1-5
 //i increases by 2 each time
 for (var i = 1; i < 10; i += 2) {
 
-  for (var j = 0; j < 9 - i / 2; j++)
+  //number of spaces is equal to i - 1 / 2 or it will grow too fast
+  for (var j = 0; j < 9 - i / 2; j++) {
     console.log(' ');
-  for (var j = 0; j < i; j++) 
+  }
+  //j is increasing by 2 each time
+  for (var j = 0; j < i; j++){
     console.log('*');
+  }
 }
-
+//top half of triangle, lines 6-9
+//i decreases by 2 each time
 for (var i = 7; i >= 0; i -= 2) {
-  
-  for (var j = 0; j < 9 - i / 2; j++)
+  //number of spaces is equal to i - 1 / 2 or it will grow too fast
+  for (var j = 0; j < 9 - i / 2; j++) {
     console.log('');
   }
+  //follows i, i is decreasing
   for (var j = 0; j < i; j++) {
   console.log('*');
   }

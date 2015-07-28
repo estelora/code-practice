2d3b18078fe73f265406da785575bf3f115e1241
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-07-28 09:27:17 -0400

    add second solution primeSieve.js

--- evenCheck.js
+++ evenCheck.js
@@ -3,7 +3,11 @@ function evenCheck(int) {
     return true;
   } else if (int === 1) {
     return false;
+
+  //remember the negative numbers!
   } else if (int < 0) {
+
+  //recursive call to reverse the negative
     return evenCheck(-int);
   } else {
     return evenCheck(int - 2);
--- primeSieve.js
+++ primeSieve.js
@@ -27,5 +27,33 @@ function primeFilter(numbers) {
   console.log(primes);
 }
 
+//Solution with counting by 2, starting at 3.
+
+var primeSieve = primeFilter(allNumbers);
+
+
+var allNumbers = [];
+var rangeEnd = 100;
+
+for (var i = 1; i <= rangeEnd; i++) {
+   allNumbers.push(i);
+}
+
+var lengthArray = allNumbers.length;
+
+function primeFilter(numbers) {
+  primes = [];
+  for (var i = 3; i < lengthArray; i+= 2) {
+
+    if (i === 3 || i === 5 || i === 7 || i === 11) {
+      primes.push(i);
+    } else if ((i % 11 !== 0) && (i % 7 !== 0) && (i % 5 !== 0) && (i % 3 !== 0) && (i % 2 !== 0)) {
+      primes.push(i);
+    }
+
+  }
+  console.log(primes);
+}
+
 var primeSieve = primeFilter(allNumbers);
 

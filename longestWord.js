0d0a34e98e6fe0671525b6948fb36790b954379f
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-07-30 15:03:14 -0400

    added simpleAdding.js

--- longestWord.js
+++ longestWord.js
@@ -26,18 +26,3 @@ function LongestWord(sen) {
 
 LongestWord("my best friend");
 
-
-//same, but with a regEx
-
-function LongestWord(str) {
-    str = str.replace(/[^a-zA-Z]+/g," ");
-    var best="";
-    var arr=str.split(" ");
-    for (var i = 0; i < array.length;i++) {
-        if (best.length < arr[i].length) {
-            best=arr[i];
-        } 
-    }
-    return best;   
-}
-   
---
+++ simpleAdding.js
@@ -0,0 +1,9 @@
+function simpleAdding(num) { 
+  for (i = num - 1; i >= 1; i--) {
+  num = num + i;
+  } 
+  return num; 
+         
+}
+
+simpleAdding(3);

dfb7c71c43e74e34a61a2f87885bbb3f74724611
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-04 16:43:45 -0400

    first test complete

--- jasmine/jasmine-standalone-2.3.4/spec/helloWorld_spec.js
+++ jasmine/jasmine-standalone-2.3.4/spec/helloWorld_spec.js
@@ -1,5 +1,5 @@
 describe('helloWorld', function() {
-  it('should return HelloWorld', function(){
-    expect(helloWord()).toEqual('HelloWorld');
+  it('should return Hello World', function(){
+    expect(helloWord()).toEqual('Hello World');
   });
 });
---
+++ jasmine/jasmine-standalone-2.3.4/src/helloWord.js
@@ -0,0 +1,3 @@
+var helloWord = function() {
+  return 'Hello World';
+}; 

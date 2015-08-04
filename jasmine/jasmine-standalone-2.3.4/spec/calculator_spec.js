8f6cd91c00d6e73b156372c8055d1b1cf557a4e1
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-04 16:56:49 -0400

    added calculator testing examples

---
+++ jasmine/jasmine-standalone-2.3.4/spec/calculator_spec.js
@@ -0,0 +1,5 @@
+describe("Calculator", function(){
+  it("should add numbers", function() {
+    expect(Calculator.add(5)).toEqual(5);
+  });
+});
---
+++ numberFun/calculator.js
@@ -0,0 +1,6 @@
+window.Calculator = {
+  add:function(num1) {
+    return 0 + num1;
+  }
+
+};

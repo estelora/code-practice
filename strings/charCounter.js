e6d146957295c9df3fa1818ee491a4e6c77bc947
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-06 14:29:01 -0400

    removed unnecessary code from longestWord.js

--- strings/charCounter.js
+++ strings/charCounter.js
@@ -1,22 +1,4 @@
-//counts the number of occurences of a given character in a string
-
-function charCounter(string, char) {
-  var count = 0;
-  
-  for (var i = 0; i < string.length; i++){
-    if (string.charAt(i) == char)
-      count += 1;
-  }
-  return count;
-}
-
-charCounter('my Company', 'c'); //returns 0 because of case sensitivity
-
-charCounter('lydia', 'l');
-
-charCounter('me', 'i');
-
-
+//counts # of incidences of a char in a given str
 //case-insensitive version
 
 function charCounter(string, char) {
--- strings/longestWord.js
+++ strings/longestWord.js
@@ -13,6 +13,7 @@ function longestWord(sen) {
     });
     return word;
 }
+
 longestWord('tummy yummy');
 
 
@@ -34,40 +35,5 @@ longestWord('pride and prejudice');
 
 longestWord('');
 
-
-
-
-
 longestWord('my best friend');
-
-function LongestWord(sen) {
-  longest = null;
-  words = sen.split(' ');
-  
-  //forEach instead of a for Loop
-  words.forEach(function(word){
-    if (word.length > longest.length){
-        longest = word;
-    };
-  });
-return longest;
-};
-
-LongestWord("the Longest word");
-
-function LongestWord(sen) {
-    longest = null;
-    words = sen.split(' ');
-      
-    //for Loop instead of forEach
-    for (var i = 0; i < words.length; i++) {
-    if (longest.length < words[i].length) {
-        longest = words[i];
-        } 
-    }
-    return longest;   
-}
-
-LongestWord("my best friend");
-
 

70c1c461822fdc9afa9bd60f44b568d53fd0704f
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-06 16:28:41 -0400

    added simpleAdding_spec.js

---
+++ jasmine/jasmine-standalone-2.3.4/spec/simpleAdding_spec.js
@@ -0,0 +1,8 @@
+describe('simpleAdding', function () {
+
+  it('should return the sum of the numbers and all numbers below', function () {
+    expect(simpleAdding(5)).toEqual(15);
+
+  });
+
+});
--- jasmine/jasmine-standalone-2.3.4/spec/sumForEach_spec.js
+++ jasmine/jasmine-standalone-2.3.4/spec/sumForEach_spec.js
@@ -1,6 +1,6 @@
 describe('sumForEach', function () {
 
-  it('should return the sum of all numbers', function () {
+  it('should return the sum of all numbers in the constructed array', function () {
     expect(sumForEach(6)).toEqual(21);
 
   });

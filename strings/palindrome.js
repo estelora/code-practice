eb5a430def8a43b8f090e1fb168c65a9921c50de
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-09 14:49:31 -0400

    added regexPalindrome.js solution

--- strings/palindrome.js
+++ strings/palindrome.js
@@ -14,3 +14,14 @@ isPalindrome('hello');
 isPalindrome('racecar');
 isPalindrome('avid diva');
 isPalindrome('is this a palindrome?');
+
+//this Example uses regex and works even with strange spaces, not mine
+function Palindrome(str) { 
+  var lets = str.match(/[a-z]/ig).join('').toLowerCase();
+  function isPalin(c){
+    if(c.length <= 1) return true;
+    if(c[0] !== c[c.length-1]) return false;
+    return isPalin(c.substr(1,c.length-2));
+  }
+  return isPalin(lets);
+}

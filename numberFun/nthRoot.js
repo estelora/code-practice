2d14af10a5032e1c16249433a70a0acf81ba3dc9
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-12 11:28:05 -0400

    completed nthRoot.js

--- numberFun/nthRoot.js
+++ numberFun/nthRoot.js
@@ -1,3 +1,13 @@
+//write a function that finds the nth root of a given number
+
+
 function nthRoot(number, root) {
-  
+  var result = Math.pow(number, 1/root);
+  return result;
 };
+
+nthRoot(4, 2);
+//return 2, square root of 4 is 2
+
+nthRoot(8, 3);
+//return 2, cube root of 8 is 2

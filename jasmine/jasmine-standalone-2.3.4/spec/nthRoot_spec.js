b213f663229bd9f90e72de6a6a57c6fa0e763aca
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-12 11:32:59 -0400

    added unit test for nthRoot.js

---
+++ jasmine/jasmine-standalone-2.3.4/spec/nthRoot_spec.js
@@ -0,0 +1,9 @@
+describe('nthRoot', function () {
+
+  it('should return the nth root of a number', function () {
+    expect(nthRoot(4, 2)).toEqual(2);
+    expect(nthRoot(8, 3)).toEqual(2);
+
+  });
+
+});

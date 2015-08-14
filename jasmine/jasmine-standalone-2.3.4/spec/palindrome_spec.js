d7865675cc7f900c3493663d4f3f0f432476a21e
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-14 13:55:30 -0400

    first toBeTruthyTest, palindrome_spec.js added to tests

---
+++ jasmine/jasmine-standalone-2.3.4/spec/palindrome_spec.js
@@ -0,0 +1,11 @@
+describe('isPalindrome', function () {
+
+  it('should return true when a string is a palindrome', function () {
+    expect(isPalindrome(racecar)).toBeTruthy();
+
+  });
+
+});
+
+
+//expect(12).toBeTruthy();

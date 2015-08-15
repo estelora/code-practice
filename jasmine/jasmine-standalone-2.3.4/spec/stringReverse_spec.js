534122c4a6e2ec47c6c899a23945b3b46c860683
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-15 11:46:38 -0400

    added stringReverse_spec.js, all tests pass

---
+++ jasmine/jasmine-standalone-2.3.4/spec/stringReverse_spec.js
@@ -0,0 +1,10 @@
+describe('stringReverse', function () {
+
+  it('should return the reversed version of a string', function () {
+    expect(stringReverse('honeybunny')).toEqual('ynnubyenoh');
+    expect(stringReverse('Backwords')).toEqual('sdrowkcaB');
+    expect(stringReverse('palindromE')).toEqual('Emordnilap');
+    expect(stringReverse('racecar')).toEqual('racecar');
+  });
+
+});

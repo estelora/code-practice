7527d6a3ada0b11736307a59f1696967899e2de4
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-15 12:00:40 -0400

    set up arithGeo.js & arithGeo_spec.js

---
+++ jasmine/jasmine-standalone-2.3.4/spec/arithGeo_spec.js
@@ -0,0 +1,7 @@
+describe('arithGeo', function () {
+it ('should indicate whether an array of numbers 
+     follows a geometric or arithmetic pattern',
+     function() {
+
+     }; 
+});
---
+++ numberFun/arithGeo.js
@@ -0,0 +1,30 @@
+// Determine if an array of numbers follows a
+// geometric or arithmetic pattern
+// Geometric pattern, where a given number 
+// will be divisible by a previous number
+// or an arithmetic pattern, 
+//where each element increases by a constant amount.
+
+function arithGeo(array) {
+  //check if it's actually an array
+  //check length, must be greater than 1
+  //determine if it's a geo or arith pattern by comparing #'s
+
+
+
+  if() {
+  //arith
+  } 
+  else if() {
+  //geometric
+  }
+  else if {
+  //both
+  } else {
+    //neither
+    return -1;
+  }
+
+
+}
+

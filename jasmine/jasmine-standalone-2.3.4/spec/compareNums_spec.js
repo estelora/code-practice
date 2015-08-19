e14eaa43d8c6ab53e47567f0ff31b12f0e36bc2d
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-19 17:21:36 -0400

    add boundary test cases, clean up documentation

--- jasmine/jasmine-standalone-2.3.4/spec/compareNums_spec.js
+++ jasmine/jasmine-standalone-2.3.4/spec/compareNums_spec.js
@@ -1,8 +1,12 @@
 describe('compareNums', function(){
-  it('should compare 2 nums return false if first num > second num,return true if second num > first num, -1 if nums are the same value', function() {
-      expect(compareNums(1, 10)).toBeTruthy();
+  it('should compare 2 nums return false if first num > second num, return true if second num > first num, -1 if nums are equal', function() {
+      expect(compareNums(1000, 5000)).toBeTruthy();
+      expect(compareNums(0, 1)).toBeTruthy();
+      expect(compareNums(0, -10)).toBeFalsy();
       expect(compareNums(55, 1)).toBeFalsy();
+       expect(compareNums(55, -1)).toBeFalsy();
       expect(compareNums(5, 5)).toEqual(-1);
+      expect(compareNums(-7.3, -7.3)).toEqual(-1);
     });
 
 });

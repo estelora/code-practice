b5fd760338de691df355d8fb4364216ee8fd3f3e
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-19 17:10:36 -0400

    fixed syntax error in compareNums_spec.js

--- jasmine/jasmine-standalone-2.3.4/spec/compareNums_spec.js
+++ jasmine/jasmine-standalone-2.3.4/spec/compareNums_spec.js
@@ -1,10 +1,5 @@
 describe('compareNums', function(){
-  it('should compare first num and second num, 
-    return false if first num > second num,
-    return true if second num > first num,
-    -1 if nums are the same value',
-    function() {
-
+  it('should compare 2 nums return false if first num > second num,return true if second num > first num, -1 if nums are the same value', function() {
       expect(compareNums(1, 10)).toBeTruthy();
       expect(compareNums(55, 1)).toBeFalsy();
       expect(compareNums(5, 5)).toEqual(-1);

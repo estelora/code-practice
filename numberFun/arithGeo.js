ce706018377acf0cc0b6c2bb591c2362b1b28d9e
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-20 14:55:07 -0400

    completed arithGeo.js

--- numberFun/arithGeo.js
+++ numberFun/arithGeo.js
@@ -1,27 +1,60 @@
 // Determine if an array of numbers follows a
-// geometric or arithmetic pattern
-// Geometric pattern, where a given number 
-// will be divisible by a previous number
-// or an arithmetic pattern, 
-//where each element increases by a constant amount.
+// geometric (return 'Geometric')
+// or arithmetic pattern (return 'Arithmetic')
+// both (return 'both')
+// or neither (return -1)
+// Geometric pattern =  a given number is divisible by the previous number
+// Arithmetic pattern = where each number element increases by a constant amount. 
 
 function arithGeo(array) {
-  //check if it's actually an array
-  //check length, must be greater than 1
-  //determine if it's a geo or arith pattern by comparing #'s
 
-
-
-  if() {
-  //arith
-  } 
-  else if() {
-  //geometric
+  //array cannot be empty
+  if(array == null || !Array.isArray(array)){
+    return "error";
   }
-  else if {
+
+  var length = array.length;   
+    //if length is 0, neither is true
+    if(length === 0){
+      return "neither";
+    }
+    // if length 1, both are true
+    if(length === 1){
+      return "both";
+    }
+
+  //arithmetic
+  var difference = array[1] - array[0];
+  //geometric
+  var ratio = array[1] / array[0];
+  //count variables
+  var countArithmetic = 0;
+  var countGeometric = 0;
+
+  //determine if it's a geo or arith pattern by comparing #'s
+  for (var i = 1; i < array.length; i++ ) {
+    if((array[i] - array[i-1]) === difference){
+      countArithmetic++;
+    }
+    if((array[i] / array[i-1]) === ratio){
+      countGeometric++;
+    }
+  }
+
+  //check counts to compare
+  //return 'Arithmetic', 'Geometric', 'both', or -1 (neither)
+  if(countArithmetic === length-1) {
+    return 'Arithmetic';
+  } 
+  //geometric
+  else if(countGeometric === length-1) {
+    return 'Geometric';
+  }
   //both
+  else if((countArithmetic === length-1) && (countGeometric === length-1)) {
+    return 'both';
+  //neither
   } else {
-    //neither
     return -1;
   }
 

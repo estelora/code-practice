4bf5b4dd3269f7aee9cf659a9d39528ec6172113
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-20 15:05:24 -0400

    added test cases to arithGeo_spec.js, all tests are apassing

--- jasmine/jasmine-standalone-2.3.4/spec/arithGeo_spec.js
+++ jasmine/jasmine-standalone-2.3.4/spec/arithGeo_spec.js
@@ -1,7 +1,13 @@
 describe('arithGeo', function () {
-it ('should indicate whether an array of numbers 
-     follows a geometric or arithmetic pattern',
+it ('should indicate whether an array of numbers follow a geometric or arithmetic pattern',
      function() {
+      expect(arithGeo([1, 2, 3 , 4])).toEqual('Arithmetic');
+      expect(arithGeo([0])).toEqual('both');
+      expect(arithGeo([])).toEqual('neither');
+      expect(arithGeo(null)).toEqual('error');
+      expect(arithGeo([2, 4, 8, 16])).toEqual('Geometric');
+      expect(arithGeo([3, 2, 6])).toEqual(-1);
+  
 
-     }; 
+     }); 
 });

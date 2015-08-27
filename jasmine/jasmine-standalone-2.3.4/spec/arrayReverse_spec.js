7ad2f0b50c04c2b5ba4cd60e004326faa6fd2813
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-26 20:08:18 -0400

    fixed syntax error in arrayReverse_spec.js

--- jasmine/jasmine-standalone-2.3.4/spec/arrayReverse_spec.js
+++ jasmine/jasmine-standalone-2.3.4/spec/arrayReverse_spec.js
@@ -1,5 +1,5 @@
 describe('arrayReverse', function() {
-  it should('return the reversed version of a given array',
+  it('should return the reversed version of a given array',
    function() {
     expect(arrayReverse([1, 2, 3, 4, 5, 6]).toEqual([6,5,4,3,2,1]);
    });

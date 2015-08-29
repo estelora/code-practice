b4788dfbad73870f8d2f134cbc21a131dd1f934a
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-29 14:31:26 -0400

    fixed arraySum_spec.js, all tests passing

--- jasmine/jasmine-standalone-2.3.4/spec/arraySum_spec.js
+++ jasmine/jasmine-standalone-2.3.4/spec/arraySum_spec.js
@@ -1,6 +1,6 @@
 //test not working :( maybe type conflict, not sure 
 describe('arraySum', function() {
   it('should return the sum of all values in an array', function() {
-    expect([1,2,3,4,5]).toEqual(15);
+    expect(arraySum([1,2,3,4,5])).toEqual(15);
   });
 });

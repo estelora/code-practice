fe49b4d115bb403b09dcff03a1e27759f25274bb
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-29 13:41:29 -0400

    continue working on jasmine tutorial, added example_spec.js & example.js, restructured order in SpecRunner.html

---
+++ jasmine/jasmine-standalone-2.3.4/spec/example_spec.js
@@ -0,0 +1,8 @@
+describe('some feature', function() {
+  describe('someFunction', function(){
+    it('should return true when called', function() {
+      expect(someFunction()).toEqual(true);
+      
+    });
+  });
+});

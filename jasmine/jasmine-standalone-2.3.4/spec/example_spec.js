9a3a8fe04c5f00a9a0ed46299f70dd3653986170
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-29 13:51:58 -0400

    added toBeTruthy, toBeFalsy, .toEqual, and .not examples to example_spec.js

--- jasmine/jasmine-standalone-2.3.4/spec/example_spec.js
+++ jasmine/jasmine-standalone-2.3.4/spec/example_spec.js
@@ -1,8 +1,19 @@
 describe('some feature', function() {
   describe('someFunction', function(){
     it('should return true when called', function() {
-      expect(someFunction()).toEqual(true);
+      /* toEqual() works but is not the best matcher
+       * expect(someFunction()).toEqual(true); */
       
+      // better matcher for a boolean
+      expect(someFunction()).toBeTruthy();
+      
+      /* matcher for expecting false 
+       * expect(someFunction()).toBeFalsy(); */
+      
+      /* .not.toBeTruthy negates, aka toBeFalsy
+       * expect(someFunction()).not.toBeTruthy();
+       * .not.toBeFalsy negates, aka toBeTruthy
+       * expect(someFunction()).not.toBeFalsy(); */
     });
   });
 });

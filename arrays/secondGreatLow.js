8f7147866fb98b5daf35274aae3424f5d86da60a
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-29 20:38:48 -0400

    secondGreatLow.js almost done

--- arrays/secondGreatLow.js
+++ arrays/secondGreatLow.js
@@ -1,15 +1,38 @@
 function secondGreatLow(array) {
-  //sort array, ascending
-  var sort = array.sort(function(a, b){return a-b});
-  //remove lowest #
-  sort.splice(0,1);
-  var last = sort.length-1;
-  sort.splice(last, 1);
-  console.log(sort);
-  var secondLowest = sort[0];
-  console.log(secondLowest);
-  var secondGreatest = sort[sort.length-1];
-  console.log(secondGreatest);
+  var sort;
+  var input = array;
+  if (input.length == 2 ) {
+    sort = input.sort(function(a, b){return a-b});
+    var finalArray = sort;
+    return finalArray;
+
+
+  } else if (input.length == 3) {
+    var sort = input.sort(function(a, b){return a-b});
+    //remove lowest #
+     sort.splice(0,1);
+    var last = sort.length-1;
+    //remove highest #
+    sort.splice(last, 1);
+    var result = sort.toString() + ', ' + sort.toString();
+    console.log(result);
+    
+
+
+  } else if (input.length == 2) {
+     //sort array, ascending
+    var sort = input.sort(function(a, b){return a-b});
+    //remove lowest #
+    sort.splice(0,1);
+    var last = sort.length-1;
+    //remove highest #
+    sort.splice(last, 1);
+    //return new array
+    sort = [sort[0], sort[sort.length-1]];
+    var finalArray = sort;
+    return finalArray;
+  }
+
 }
 
-secondGreatLow([1,2,6,3,5,4]);
+secondGreatLow([1,2,6]);

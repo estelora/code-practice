8b2d138e0e6b440edf8039271d3404368ef96eea
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-29 21:00:37 -0400

    secondGreatLow.js, passing most test cases

--- arrays/secondGreatLow.js
+++ arrays/secondGreatLow.js
@@ -1,10 +1,10 @@
-function secondGreatLow(array) {
+function SecondGreatLow(array) {
   var sort;
   var input = array;
   if (input.length == 2 ) {
     sort = input.sort(function(a, b){return a-b});
-    var finalArray = sort;
-    return finalArray;
+    var result = sort[0] + ' ' + sort[1];
+    return result;
 
 
   } else if (input.length == 3) {
@@ -14,25 +14,28 @@ function secondGreatLow(array) {
     var last = sort.length-1;
     //remove highest #
     sort.splice(last, 1);
-    var result = sort.toString() + ', ' + sort.toString();
+    var result = sort.toString() + ' ' + sort.toString();
     console.log(result);
+    return result;
     
 
 
-  } else if (input.length == 2) {
+  } else if (input.length > 3) {
      //sort array, ascending
     var sort = input.sort(function(a, b){return a-b});
     //remove lowest #
+    console.log(sort);
     sort.splice(0,1);
     var last = sort.length-1;
     //remove highest #
     sort.splice(last, 1);
     //return new array
     sort = [sort[0], sort[sort.length-1]];
-    var finalArray = sort;
-    return finalArray;
+    var result = sort[0] + ' '  + sort[1];
+    return result;
   }
 
-}
-
-secondGreatLow([1,2,6]);
+} 
+//incorrect test cases :(
+//[7, 7, 90, 1000003]
+//(2,2,2,5,5,5,6)

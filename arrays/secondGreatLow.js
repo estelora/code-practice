0eacc96ece3d9b8d6c3f14c0acac844aae95b7cd
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-30 17:41:16 -0400

    much better solution for secondGreatLow.js

--- arrays/secondGreatLow.js
+++ arrays/secondGreatLow.js
@@ -1,43 +1,47 @@
-function SecondGreatLow(array) {
+
+function secondGreatLow(array) {
   var sort;
   var input = array;
   //sort array, ascending
   var sort = input.sort(function(a, b){return a-b});
   //remove duplicates
-  /* var unique =  = a.filter(function(item, pos) {
-    return a.indexOf(item) == pos;
-  })*/
-  if (sort.length == 2 ) {
-    var result = sort[0] + ' ' + sort[1];
+  var unique = sort.filter(function(item, pos) {
+    return sort.indexOf(item) == pos;
+  });
+  console.log(unique);
+  if (unique.length == 1 ) {
+    var result = unique[0] + ' ' + unique[0];
     return result;
 
+  } else if (unique.length == 2 ) {
+    var result = unique[0] + ' ' + unique[1];
+    return result;
 
-  } else if (sort.length == 3) {
+  } else if (unique.length == 3) {
     //remove lowest #
-     sort.splice(0,1);
-    var last = sort.length-1;
+    unique.splice(0,1);
+    var last = unique.length-1;
     //remove highest #
-    sort.splice(last, 1);
-    var result = sort.toString() + ' ' + sort.toString();
+    unique.splice(last, 1);
+    var result = unique.toString() + ' ' + unique.toString();
     console.log(result);
     return result;
     
 
 
-  } else if (sort.length > 3) {
+  } else if (unique.length > 3) {
     //remove lowest #
-    console.log(sort);
-    sort.splice(0,1);
-    var last = sort.length-1;
+    unique.splice(0,1);
+    var last = unique.length-1;
     //remove highest #
-    sort.splice(last, 1);
+    unique.splice(last, 1);
     //return new array
-    sort = [sort[0], sort[sort.length-1]];
-    var result = sort[0] + ' '  + sort[1];
+    var result = unique[0] + ' '  + unique[1];
     return result;
   }
 
 } 
 //incorrect test cases :(
-//[7, 7, 90, 1000003]
-//(2,2,2,5,5,5,6)
+//(78, 90, 100, 1, 2)
+//(100, 200, 3, 400, 5, 1) 
+//(90, 23)
